b40e586b0eb0f67a4c86687111a951648d28d918
IlyaShipov <i.shipov2016@yandex.ru>
2022-09-07 14:58:26 +0300

    Hello World!

--- Shipov Lab_0/Shipov Lab_0/Main.cpp
+++ Shipov Lab_0/Shipov Lab_0/Main.cpp
@@ -1,16 +1,6 @@
 #include <iostream>;
 
-using namespace std;
-
-unsigned char a;
-
 int main()
 {
-	setlocale(LC_ALL, "Rus");
-	cout << "Hello World!\n";
-	/*cout << "ֲגוהטעו סטלגמכ: ";
-	cin >> a;
-	cout << "ֲגוהוםûי סטלגמכ: " << a << "\n";
-	system("PAUSE");
-	return 0;*/
+	std::cout << "Hello World!\n";
 }
